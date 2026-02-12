0b805bf5e548af91372938d12d6696ad059e6cc0
Hirusha Fernando <heshanhfernando@gmail.com>
2026-02-12 16:28:01 +0530

    Project Added

--- public/assets/js/data.js
+++ public/assets/js/data.js
@@ -210,6 +210,24 @@ window.portfolioData = {
             "thumbnail": "/public/assets/images/Spex-1.png"
         },
         {
+            "title": "Blog Writing Assistant",
+            "description": "Agent-based AI assistant for Medium blog writing that enhances existing articles by improving introductions and conclusions, correcting grammar and spelling, generating SEO-friendly titles and descriptions, and applying clean Markdown formatting",
+            "tags": [
+                "CrewAI",
+                "Streanmlit",
+                "Python",
+                "OpenAI API"
+            ],
+            "github": [
+                {
+                    "url": "https://github.com/HRS0986/medium-blog-assistant",
+                    "title": "Code"
+                }
+            ],
+            "live": "#",
+            "thumbnail": "/public/assets/images/medium.png"
+        },
+        {
             "title": "ZoteroPDF",
             "description": "A Zotero plugin that enables you to export your library items and collections as PDF documents with ease",
             "tags": [
